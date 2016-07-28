6c88eeb7a93d75e3827553f8c9e6ae5ce5ccd4eb
Alessio Lentini <>
2016-07-28 17:54:57 +0200

    Rotation and (stub) strafing

--- js/direction.js
+++ js/direction.js
@@ -1,34 +1,66 @@
 var fullChangeDirectionTime = 500;
-var fullAngle = Math.PI/4;
-var speedDirection = fullChangeDirectionTime/10000;
+var fullAngle = Math.PI/8;
+var speedDirection = 1/5;
 var i;
 
 
 function updateRotation(player, delta, xMovement, yMovement){
     updateXRotation(player, delta, xMovement);
-    //updateYRotation(player, delta, yMovement);
+    updateYRotation(player, delta, yMovement);
 }
 
 function updateXRotation(player, delta, xMovement){
+    
     var direction = player.xDirection;
+    var multiplier = 1;
     if(direction > 0)
         var centerDirection = -1;
     else if(direction < 0)
         var centerDirection = +1;
-    else centerDirection = 0;
-    var centeringEffect = centerDirection * speedDirection * delta;
+    else {centerDirection = 0;
+        multiplier = 2;
+    }
+    var centeringEffect = centerDirection *speedDirection * delta;
     
     if(xMovement == 0){
-        if(Math.abs(direction) < Math.abs(centeringEffect)){
+        oldDirection = direction;
+        direction += centerDirection *speedDirection * delta;
+        
+        if((oldDirection > 0 && direction < 0) || (oldDirection < 0 && direction > 0))
             direction = 0;
-        }
-    } else {
-        var inputDirection = xMovement * 2*speedDirection * delta;
-        console.log(inputDirection);
-        direction += inputDirection + centeringEffect;
+    } else {        
+        direction += xMovement * speedDirection * delta;
     }
     
     direction = Math.max(-1, Math.min(1, direction));
     player.xDirection = direction;
+    player.rotation.z = direction * fullAngle;
+}
+
+function updateYRotation(player, delta, yMovement){
+    
+    var direction = player.yDirection;
+    var multiplier = 1;
+    if(direction > 0)
+        var centerDirection = -1;
+    else if(direction < 0)
+        var centerDirection = +1;
+    else {centerDirection = 0;
+        multiplier = 2;
+    }
+    var centeringEffect = centerDirection *speedDirection * delta;
+    
+    if(yMovement == 0){
+        oldDirection = direction;
+        direction += centerDirection *speedDirection * delta;
+        
+        if((oldDirection > 0 && direction < 0) || (oldDirection < 0 && direction > 0))
+            direction = 0;
+    } else {        
+        direction += yMovement * speedDirection * delta;
+    }
+    
+    direction = Math.max(-1, Math.min(1, direction));
+    player.yDirection = direction;
     player.rotation.x = direction * fullAngle;
 }
