5e55121ac7e93ce6636bbb128d371470e4232972
Utente <Utente@Utente-PC>
2016-07-31 20:23:02 +0200

    Obstacles manager updated.

--- js/obstacles.js
+++ js/obstacles.js
@@ -1,9 +1,11 @@
 function Obstacles(threshold){
     this.obstacles_array = [];
     this.threshold = threshold;
+    this.spawnTime = 5;
+    this.elapsedTime = 0;
     
     this.add = function(o){
-        obstacles_array.push(o);
+        this.obstacles_array.push(o);
     }
     
     this.despawn = function(){
@@ -15,8 +17,24 @@ function Obstacles(threshold){
     }
     
     this.update = function(speed, delta){
-        for(var i = 0; i<obstacles_array.length; i++){
-            obstacles_array[i].position.z -= speed * delta;
+        for(var i = 0; i<this.obstacles_array.length; i++){
+            this.obstacles_array[i].position.z -= speed * delta;
+            this.obstacles_array[i].material.alpha = 1+(this.obstacles_array[i].position.z/5);
         }
+    }
+    
+    this.spawn = function(scene, delta){
+        this.elapsedTime += delta;
+        if(this.elapsedTime < this.spawnTime)
+            return;
+        this.elapsedTime = 0;
+        this.spawnTime -= this.spawnTime/100;
+        console.log(this.spawnTime);
+        var sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 2, scene);
+        sphere.material = new BABYLON.StandardMaterial("", scene);
+        sphere.position.z = 10;
+        sphere.position.x = Math.random() *2;
+        sphere.position.y = Math.random();
+        this.add(sphere);
     }
 }
